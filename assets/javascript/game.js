b5892dd7f6ab8e9a2507c93c9caa1a167d8ef840
Stacy Holtz <stacyholtz6@gmail.com>
2019-10-20 18:29:36 -0600

    working on addition

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
   // Variables
   var targetScore = "";
-  var currentScore = "";
+  var currentScore = 0;
   var wins = 0;
   var losses = 0;
   var blueNumber = "";
@@ -15,55 +15,66 @@ $(document).ready(function () {
   var pink = $("#pink");
   var yellow = $("#yellow");
 
-  // blank array for gem values?
-  // var gemValues = [];
 
-
-  // generate random number
+  // generate random number for target number
   targetScore = randomTargetScore();
   // and then put random number into target score div
   $("#target-score").text(targetScore);
   console.log("target " + targetScore);
 
-  // generate random numbers when a gem is clicked 1-12 hidden value
+  // generate random gem numbers when a gem is clicked 1-12 hidden value
   blueNumber = randomGemNumber();
   blue.on("click", function () {
-    $("#current-score").text(blueNumber);
+    currentScore = currentScore + blueNumber;
+    $("#current-score").text(currentScore);
+    win();
     console.log("blue " + blueNumber);
   });
 
   greenNumber = randomGemNumber();
   green.on("click", function () {
-    $("#current-score").text(greenNumber);
+    currentScore = currentScore + greenNumber;
+    $("#current-score").text(currentScore);
+    win();
     console.log("green " + greenNumber);
   });
 
   pinkNumber = randomGemNumber();
   pink.on("click", function () {
-    pinkNumber = Math.floor(Math.random() * 12) + 1;
-    $("#current-score").text(pinkNumber);
+    currentScore = currentScore + pinkNumber;
+    $("#current-score").text(currentScore);
+    win();
     console.log("pink " + pinkNumber);
   });
 
   yellowNumber = randomGemNumber();
   yellow.on("click", function () {
-    $("#current-score").text(yellowNumber);
+    currentScore = currentScore + yellowNumber;
+    $("#current-score").text(currentScore);
+    win();
     console.log("yellow " + yellowNumber);
   });
 
-
-  // win if your score equals random number -- score checker
-  if (currentScore === targetScore) {
-    alert("You win!");
-    wins++;
-  }
-  else if (currentScore >= targetScore) {
-    alert("You lose!");
-    losses++;
+  function win() {
+    if (currentScore === targetScore) {
+      wins++;
+      $("#wins").text(wins);
+      if (confirm("You won. Would you like to play again?")) {
+        reset();
+      };
+      console.log("wins " + wins);
+    }
+    else if (currentScore >= targetScore) {
+      losses++;
+      $("#losses").text(losses);
+      if (confirm("You lost. Would you like to play again?")) {
+        reset();
+      }
+      console.log("losses " + losses);
+    }
   }
 
   // function for random numbers
-
   function randomGemNumber() {
     return Math.floor(Math.random() * 12) + 1;
   };
@@ -74,22 +85,15 @@ $(document).ready(function () {
 
   // reset function
   function reset() {
-
+    currentScore = 0;
+    targetScore = randomTargetScore();
+    blueNumber = randomGemNumber();
+    greenNumber = randomGemNumber();
+    pinkNumber = randomGemNumber();
+    yellowNumber = randomGemNumber();
+    $("#current-score").text(currentScore);
+    $("#target-score").text(targetScore);
   };
-  // HOW DO I ADD THEM ALL TOGETHER?????
-
-  // change string numbers to integers
-  // blueNumber = parseInt(blueNumber);
-  // greenNumber = parseInt(greenNumber);
-  // yellowNumber = parseInt(yellowNumber);
-  // pinkNumber = parseInt(pinkNumber);
-
-  // add the random numbers together
-
-  // display number each time it is added to
-
-
-  // create the random number 
 
 
 
